730efabdf48cad334150e8c8c66f72700e5fcfa8
ChristerNilsson <janchrister.nilsson@gmail.com>
2022-10-25 11:23:14 +0200

    added some help texts and numbering

--- src/Search.svelte
+++ src/Search.svelte
@@ -2,26 +2,35 @@
 	export let sokruta
 	export let text0
 	export let stack
-	
+
+	function push(s) {
+		let n = placeholders.length
+		if (n < 10) n = '0' + n
+		placeholders.push(n + ' ' + s)
+	}
+ 	
 	const placeholders = []
-	placeholders.push("01 Separate words with a space")
-	placeholders.push("02 Combine words with underscore (_)")
-	placeholders.push("03 Share: copies a URL to the clipboard")
-	placeholders.push("04 Download: fetches all marked images")
-	placeholders.push("05 All: marks all images")
-	placeholders.push("06 None: unmarks all images")
-	placeholders.push("07 Home is a directory button, selects all images")
-	placeholders.push("08 The listbox contains directories in the current directory")
-	placeholders.push("09 The current directory is shown in bold")
-	placeholders.push("10 Search words are named A, B and C")
-	placeholders.push("11 Sort order: ABC AB AC BC A B C")
-	placeholders.push("12 High resolution images are draggable and zoomable")
-	placeholders.push("13 Only the current directory is searched")
-	placeholders.push("14 Search is case sensitive")
-	placeholders.push("15 Searching can start anywhere in a word")
-	placeholders.push("16 Tournament numbers can be searched like T10370")
-	placeholders.push("17 Member numbers can be searched like M585772")
-	placeholders.push("18 Dates can be searched like 2022-09-17, 2022-09, 2022 or 09-17")
+	push("[Clear] Show next help text")
+	push("[Share] Show previous help text")
+	push("Separate words with a space")
+	push("Combine words with underscore (_)")
+	push("Search words are named A, B and C")
+	push("Sort order: ABC AB AC BC A B C")
+	push("Only the current directory is searched")
+	push("Search is case sensitive")
+	push("Searching can start anywhere in a word")
+	push("Search Tournament numbers like T10370")
+	push("Search Member numbers like M585772")
+	push("Search dates like 2022-09-17, 2022-09, 2022 or 09-17")
+	push("[Share] copies a URL to the clipboard")
+	push("[Download] fetches all marked images")
+	push("[All] marks all images")
+	push("[None] unmarks all images")
+	push("[Home] selects images in all directories")
+	push("The current directory is shown in bold")
+	push("The listbox contains directories in the current directory")
+	push("High resolution images are draggable and zoomable")
+	placeholders[0] += " of " + placeholders.length
 	
 	function clear() {
 		sokruta = ""
